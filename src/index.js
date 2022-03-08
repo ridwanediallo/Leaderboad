c830fca3fa98516d063f6c94f4ba7f1e7476836c
ridwan <ridwanediallo99@gmail.com>
2022-03-08 14:49:23 +0000

    Add addScore method

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
-import './style.css'
+import './style.css';
 
 const list = document.querySelector('.list');
-const inputName = document.querySelector('.input-name')
-const inputScore = document.querySelector('.input-score')
-const refreshBtn = document.querySelector('.refresh-btn')
-const submitBtn = document.querySelector('.submit-btn')
+const inputName = document.querySelector('.input-name');
+const inputScore = document.querySelector('.input-score');
+const refreshBtn = document.querySelector('.refresh-btn');
+const submitBtn = document.querySelector('.submit-btn');
 
 class Data {
-    constructor(name, score) {
-        this.name = name;
-        this.score = score;
-    }
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
 }
 
 class DataList {
-    constructor () {
-        this.scores = [];
-    }
+  constructor() {
+    this.scores = [];
+  }
 
-    renderScore(list) {
-      this.scores.forEach((el, i) => {
-           const item = document.createElement('li');
-           item.classList.add('item');
-           item.id = i;
-           item.textcontent = `${el.name}: ${el.score}`;
-      })
-      list.append(item);
-    }
+  renderScore(list) {
+    this.scores.forEach((el, i) => {
+      const item = document.createElement('li');
+      item.classList.add('item');
+      item.id = i;
+      item.textcontent = `${el.name}: ${el.score}`;
+    });
+    list.append(item);
+  }
 
-    
+  addScore() {
+    const name = inputName.value;
+    const score = inputScore.value;
+    const newScore = new Data(name, score);
+    this.scores.push(newScore);
+  }
 }

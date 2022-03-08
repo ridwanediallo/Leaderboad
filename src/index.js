928b6086bf70234f912079201e35e8e7f7c4d1f9
ridwan <ridwanediallo99@gmail.com>
2022-03-08 22:35:05 +0000

    Add background color item

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const refreshBtn = document.querySelector('.refresh-btn');
 const submitBtn = document.querySelector('.submit-btn');
 
 class Data {
-  constructor(name, score) {
+  constructor(name, score, index) {
     this.name = name;
     this.score = score;
   }
@@ -29,18 +29,25 @@ class DataList {
   };
 
   renderScore(list) {
-    const item = document.createElement('li');
+    list.innerHTML = '';
     this.scores.forEach((el, i) => {
+    const item = document.createElement('li');
+    if(i % 2 === 0) {
+      item.style.backgroundColor = '#ddd'
+    }
       item.classList.add('item');
       item.id = i;
       item.innerHTML = `${el.name}: ${el.score}`;
+      list.appendChild(item);
     });
-    list.append(item);
   }
 
   addScore(name, score, scores) {
-    const newScore = new Data(name, score, scores);
-    this.scores.push(newScore);
+    if (name && score) {
+      const newScore = new Data(name, score, scores);
+      this.scores.push(newScore);
+    }
+    this.saveTolocal();
     this.renderScore(list);
     console.log(this.scores);
   }
@@ -48,8 +55,15 @@ class DataList {
 
 const scoreList = new DataList();
 
+document.addEventListener('DOMContentLoaded', () => {
+  scoreList.getFromLocal(scoreList.scores);
+  scoreList.renderScore(list);
+});
+
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   scoreList.addScore(inputName.value, inputScore.value, scoreList.scores);
-})
-
+  scoreList.saveTolocal();
+  inputName.value = '';
+  inputScore.value = '';
+});

107b4e5799e2c54f22580e51c42b739d4c3b4474
ridwan <ridwanediallo99@gmail.com>
2022-03-08 13:55:52 +0000

    Add class Data and DataList

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ const inputName = document.querySelector('.input-name')
 const inputScore = document.querySelector('.input-score')
 const refreshBtn = document.querySelector('.refresh-btn')
 const submitBtn = document.querySelector('.submit-btn')
+
+class Data {
+    constructor(name, score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
+class DataList {
+    constructor () {
+        this.scores = [];
+    }
+
+}

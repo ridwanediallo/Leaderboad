5d1772ecd01087ee07545394e0faae36ec5fea92
Ridwan <ridwanediallo99@gmail.com>
2022-03-10 21:09:25 +0000

    Add renderScore method

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ const inputScore = document.querySelector('.input-score');
 const submitBtn = document.querySelector('.submit-btn');
 const refreshBtn = document.querySelector('.refresh-btn');
 const list = document.querySelector('.list');
+
+const renderScore = (list, result) => {
+  list.innerHTML = '';
+  result.sort((a, b) => b.score - a.score);
+  result.forEach((el, i) => {
+    console.log(el);
+    const item = document.createElement('li');
+    if (i % 2 === 0) {
+      item.style.backgroundColor = '#ddd';
+    }
+    item.classList.add('item');
+    item.id = i;
+    item.innerHTML = `${el.user}: ${el.score}`;
+    list.appendChild(item);
+  });
+};

ad1e0aab0484df87cb1fac95648dade7d567cf29
Ridwan <ridwanediallo99@gmail.com>
2022-03-10 21:18:47 +0000

    Add renderScore function in a module

--- src/index.js
+++ src/index.js
@@ -4,22 +4,6 @@ const submitBtn = document.querySelector('.submit-btn');
 const refreshBtn = document.querySelector('.refresh-btn');
 const list = document.querySelector('.list');
 
-const renderScore = (list, result) => {
-  list.innerHTML = '';
-  result.sort((a, b) => b.score - a.score);
-  result.forEach((el, i) => {
-    console.log(el);
-    const item = document.createElement('li');
-    if (i % 2 === 0) {
-      item.style.backgroundColor = '#ddd';
-    }
-    item.classList.add('item');
-    item.id = i;
-    item.innerHTML = `${el.user}: ${el.score}`;
-    list.appendChild(item);
-  });
-};
-
 submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   setScore(inputName.value, +inputScore.value);

e2045d53a7b620ead8337dbd3eb2255274851f2a
Ridwan <ridwanediallo99@gmail.com>
2022-03-10 20:38:25 +0000

    Select html elements

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const inputName = document.querySelector('.input-name');
+const inputScore = document.querySelector('.input-score');
+const submitBtn = document.querySelector('.submit-btn');
+const refreshBtn = document.querySelector('.refresh-btn');
+const list = document.querySelector('.list');
